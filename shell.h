5e3cf64808b23e4ea06eb40cf71db0a330a6577c
DarryZh <87570095+DarryZh@users.noreply.github.com>
2023-03-30 23:33:52 +0800

    Create shell.h

---
+++ shell.h
@@ -0,0 +1,49 @@
+#ifndef __SHELL_H__
+#define __SHELL_H__
+
+typedef long (*syscall_func)(void);
+
+#define SHELL_FUNCTION_EXPORT_CMD(name, cmd, desc)                                  \
+                const char __fsym_##cmd##_name[] __attribute__((section(".rodata.name"))) = #cmd;    \
+                __attribute__((used)) const struct sh_syscall __fsym_##cmd __attribute__((section("FSymTab")))= \
+                {                           \
+                    __fsym_##cmd##_name,    \
+                    (syscall_func)&name     \
+                };
+
+#define SHELL_FUNCTION_EXPORT(name, desc)
+
+#define SHELL_FUNCTION_EXPORT_ALIAS(name, alias, desc)
+
+#define SHELL_CMD_EXPORT(command, desc)   \
+    SHELL_FUNCTION_EXPORT_CMD(command, command, desc)
+
+#define SHELL_CMD_EXPORT_ALIAS(command, alias, desc)  \
+    SHELL_FUNCTION_EXPORT_CMD(command, alias, desc)
+
+/* system call table */
+struct sh_syscall
+{
+    const char     *name;       /* the name of system call */
+
+    syscall_func func;      /* the function address of system call */
+};
+
+/* system call item */
+struct sh_syscall_item
+{
+    struct sh_syscall_item *next;    /* next item */
+    struct sh_syscall syscall;       /* syscall */
+};
+
+extern struct sh_syscall_item *global_syscall_list;
+extern struct sh_syscall *_syscall_table_begin, *_syscall_table_end;
+
+#define SH_NEXT_SYSCALL(index)  index++
+
+/* find out system call, which should be implemented in user program */
+struct sh_syscall *sh_syscall_lookup(const char *name);
+
+int shell_system_init(void);
+
+#endif
